526bde9558718f36bc434cb3cf763482003bb352
adgryz <adamgryz9@gmail.com>
2018-10-17 21:49:52 +0200

    ELO

--- main.js
+++
@@ -1,6 +0,0 @@
-// function calcVH() {
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty("--vh", `${vh}px`);
-// }
-// calcVH();
-// window.addEventListener("resize", calcVH, true);
