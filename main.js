e569e6e2228c252d32bd7ba8d113a7c29e4f39b0
adgryz <adamgryz9@gmail.com>
2018-10-17 19:06:56 +0200

    Elo

---
+++ main.js
@@ -0,0 +1,6 @@
+// function calcVH() {
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty("--vh", `${vh}px`);
+// }
+// calcVH();
+// window.addEventListener("resize", calcVH, true);
