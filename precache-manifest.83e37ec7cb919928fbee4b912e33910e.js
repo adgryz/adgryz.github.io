f1241c6e00d0b23f5595ddd1f477f9022711eba9
adgryz <acros787@gmail.com>
2019-07-24 20:45:55 +0200

    'ELUWA'

---
+++ precache-manifest.83e37ec7cb919928fbee4b912e33910e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "9bfeab1fd1b45ac33920",
+    "url": "/static/js/main.9bfeab1f.chunk.js"
+  },
+  {
+    "revision": "e7113560e26620b6e08a",
+    "url": "/static/js/2.e7113560.chunk.js"
+  },
+  {
+    "revision": "e7113560e26620b6e08a",
+    "url": "/static/css/2.c6422f75.chunk.css"
+  },
+  {
+    "revision": "df3601cab44594ad622772b321a1477d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85cd1864dcf8f1f498b47347076e2332.js"
+  "/precache-manifest.83e37ec7cb919928fbee4b912e33910e.js"
 );
 
 workbox.clientsClaim();
