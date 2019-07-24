9f97dd78760dc49ca124871a56300864c4de8c24
adgryz <acros787@gmail.com>
2019-07-24 19:27:46 +0200

    'ELUWA'

---
+++ precache-manifest.aa73c74c48edb69841a15b8514ae7090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "6a2c7f76ea2daaa9f53e",
+    "url": "/static/js/main.6a2c7f76.chunk.js"
+  },
+  {
+    "revision": "fe9921b6d23568c5caa0",
+    "url": "/static/js/2.fe9921b6.chunk.js"
+  },
+  {
+    "revision": "fe9921b6d23568c5caa0",
+    "url": "/static/css/2.6570083f.chunk.css"
+  },
+  {
+    "revision": "8001ca616d885e1c29f18f5ab2205d34",
+    "url": "/index.html"
+  }
+];
